d0de2d37da959db3ca6fbba9d2e3f50e3c436a5f
Qi Zhi <aqz1997@gmail.com>
2022-11-24 15:52:45 +0800

    update

--- project/src/main/java/com/example/project/controller/DepositTransferController.java
+++ project/src/main/java/com/example/project/controller/DepositTransferController.java
@@ -1,30 +1,28 @@
 package com.example.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.project.model.Account;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-
+@Controller
 @RequestMapping("/deposit")
 public class DepositTransferController {
 
-//	
-//	@Autowired
-//	AccountRepository account;
-	 
+	@Autowired
+	AccountRepository account;
+
 	@GetMapping("/display")
 	@ResponseBody
-	public void print() {
-        System.out.println("HI");
-    }
-	
-//	public Long deposit(@PathVariable("accountNumber") Long accountNumber, int deposit_amount) {
-//		System.out.println(account.findAll());
-//		System.out.println("hi");
-//
-//		return accountNumber;
-//
-//	}	
+	public Long deposit(@Param("accountNumber") Long accountNumber) {
+		System.out.println(account.findAll());
+		System.out.println("hi");
+		
+		return accountNumber;
+	}
 }
