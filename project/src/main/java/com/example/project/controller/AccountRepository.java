45ab54063aa560224d36fae177acede3d418b651
jiayigan <g_jiayi@hotmail.com>
2022-11-24 18:16:16 +0800

    /Deposit (before logging in TransHist)

--- project/src/main/java/com/example/project/controller/AccountRepository.java
+++ project/src/main/java/com/example/project/controller/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.example.project.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.project.model.Account;
 
-interface AccountRepository extends JpaRepository<Account, Long> {}	
+interface AccountRepository extends JpaRepository<Account, Long> {
+}	
--- project/src/main/java/com/example/project/controller/DepositTransferController.java
+++ project/src/main/java/com/example/project/controller/DepositTransferController.java
@@ -1,28 +1,58 @@
 package com.example.project.controller;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.project.model.Account;
+import com.example.project.model.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/deposit")
+@RequestMapping("/transact")
 public class DepositTransferController {
 
 	@Autowired
-	AccountRepository account;
+	AccountRepository accountRepository;
 
-	@GetMapping("/display")
+	User user;
+    double currentBalance;
+    double newBalance;
+	
+	@GetMapping("/deposit")
 	@ResponseBody
-	public Long deposit(@Param("accountNumber") Long accountNumber) {
-		System.out.println(account.findAll());
-		System.out.println("hi");
-		
-		return accountNumber;
+	public Long deposit(@RequestParam("accountNumber") Long accountNumber 
+										) {
+		Long deposit_amount = (long) 5;
+		if(deposit_amount == 0) {
+			System.out.println("Error: Deposit Amount cannot be 0");
+			return null;
+		}
+		               
+        BigDecimal balance;
+
+		Optional<Account> account = accountRepository.findById(accountNumber);
+        balance = account.get().getBalance();
+        
+        BigDecimal newBalance = balance.add(new BigDecimal(deposit_amount));
+        System.out.println(newBalance);
+
+        account.ifPresent(x -> x.setBalance(newBalance));
+        accountRepository.save(account.get());	
+        
+              
+        return null;
+
 	}
 }
