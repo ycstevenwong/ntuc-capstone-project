36361462165a6f0a1dc2dab4415f019513e67524
Qi Zhi <aqz1997@gmail.com>
2022-11-29 15:26:00 +0800

    Transaction History Page w functionality

--- project/src/main/java/com/example/project/controller/TransactionController.java
+++ project/src/main/java/com/example/project/controller/TransactionController.java
@@ -2,6 +2,8 @@ package com.example.project.controller;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.project.model.Account;
+import com.example.project.model.AccountType;
+import com.example.project.model.Customer;
 import com.example.project.model.Transaction;
 import com.example.project.repository.AccountRepo;
 import com.example.project.repository.TransactionRepo;
@@ -28,14 +33,22 @@ public class TransactionController {
 	@Autowired
 	TransactionRepo transactionRepo;
 
-	@GetMapping("/deposit")
+	Boolean transactionCompletion = false;
+	Boolean isFirstLoad = true; 
+
+	// End point for deposit logic that reroute back to main page
+	@GetMapping("/Deposit")
 	public String deposit(@RequestParam("accountNumber") Long accountNumber,
 			@RequestParam("amount") BigDecimal amount) {
 
+		isFirstLoad = false;
 		// Deposit Amount must be MORE THAN ZERO
 		if (amount.equals(new BigDecimal("0"))) {
-			return "ErrorPage";
+			transactionCompletion = false;
+			return "redirect:/account/transaction/display?accountNum=" + accountNumber;
 		}
+
+		transactionCompletion = true;
 
 		Optional<Account> account = accountRepository.findById(accountNumber);
 		Transaction transactionRecord = new Transaction();
@@ -51,19 +64,24 @@ public class TransactionController {
 		transactionRepo.save(transactionRecord);
 		accountRepository.save(account.get());
 
-		return "SuccessPage";
+		return "redirect:/account/transaction/display?accountNum=" + accountNumber;
 	}
 
-	@GetMapping("/withdraw")
+	// End point for withdraw logic that reroute back to main page
+	@GetMapping("/Withdraw")
 	public String withdraw(@RequestParam("accountNumber") Long accountNumber,
 			@RequestParam("amount") BigDecimal amount) {
 
 		Optional<Account> account = accountRepository.findById(accountNumber);
 
+		isFirstLoad = false;
 		// Balance amount must be MORE THAN withdrawn amount
 		if (amount.compareTo(account.get().getBalance()) == 1) {
-			return "ErrorPage";
+			transactionCompletion = false;
+			return "redirect:/account/transaction/display?accountNum=" + accountNumber;
 		}
+
+		transactionCompletion = true;
 
 		Transaction transactionRecord = new Transaction();
 		// Set transaction field based on desired value
@@ -77,9 +95,10 @@ public class TransactionController {
 		transactionRepo.save(transactionRecord);
 		accountRepository.save(account.get());
 
-		return "SuccessPage";
+		return "redirect:/account/transaction/display?accountNum=" + accountNumber;
 	}
 
+	// Main Display Page for transaction history
 	@GetMapping("/display")
 	public String display(@Param("accountNum") Long accountNum, Model model) {
 		List<Transaction> transactionList = (List<Transaction>) transactionRepo.findAll();
@@ -88,19 +107,35 @@ public class TransactionController {
 			if (transactionRecord.getAccount().getAccountNumber().equals(accountNum))
 				filteredList.add(transactionRecord);
 		}
+		// sort based on time
+		Collections.sort(filteredList, new Comparator<Transaction>() {
+			public int compare(Transaction t1, Transaction t2) {
+				return t2.getTime().compareTo(t1.getTime());
+			}
+		});
+
 		// fetch all accounts available
 		List<Account> accountList = accountRepository.findAll();
 
-
-		// fetch current account balance
+		// fetch current account details
 		Optional<Account> account = accountRepository.findById(accountNum);
 		BigDecimal currentBalance = account.get().getBalance();
+		Customer accountDetails = account.get().getCustomer();
+		String accountType = account.get().getAccountType().getName();
+		String accountNRIC = account.get().getCustomer().getNric().substring(5);
 
 		model.addAttribute("currentBalance", currentBalance);
 		model.addAttribute("transactionList", filteredList);
 		model.addAttribute("accountNumber", accountNum);
-		model.addAttribute("accountList", accountList);
+		model.addAttribute("accountNRIC", accountNRIC);
 
+		model.addAttribute("accountList", accountList);
+		model.addAttribute("accountDetails", accountDetails);
+		model.addAttribute("accountType", accountType);
+		
+		// Boolean Condition for alert box
+		model.addAttribute("transactionCompletion", transactionCompletion);
+		model.addAttribute("isFirstLoad", isFirstLoad);
 
 		return "TransactionHistory";
 	}
