f448339598aecdf503ebf1122865c3efbb93a412
Qi Zhi <aqz1997@gmail.com>
2022-11-30 16:47:10 +0800

    bug fix

--- project/src/main/java/com/example/project/controller/TransactionController.java
+++ project/src/main/java/com/example/project/controller/TransactionController.java
@@ -17,6 +17,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,7 +31,7 @@ import com.example.project.repository.AccountRepo;
 import com.example.project.repository.TransactionRepo;
 
 @Controller
-@RequestMapping("/account/transaction")
+@RequestMapping("/account")
 public class TransactionController {
 
 	@Autowired
@@ -50,7 +51,7 @@ public class TransactionController {
 		// Deposit Amount must be MORE THAN ZERO
 		if (amount.equals(new BigDecimal("0"))) {
 			transactionCompletion = false;
-			return "redirect:/account/transaction/display?accountNum=" + accountNumber;
+			return "redirect:/account/" + accountNumber;
 		}
 
 		transactionCompletion = true;
@@ -69,7 +70,7 @@ public class TransactionController {
 		transactionRepo.save(transactionRecord);
 		accountRepository.save(account.get());
 
-		return "redirect:/account/transaction/display?accountNum=" + accountNumber;
+		return "redirect:/account/" + accountNumber;
 	}
 
 	// End point for withdraw logic that reroute back to main page
@@ -83,7 +84,7 @@ public class TransactionController {
 		// Balance amount must be MORE THAN withdrawn amount
 		if (amount.compareTo(account.get().getBalance()) == 1) {
 			transactionCompletion = false;
-			return "redirect:/account/transaction/display?accountNum=" + accountNumber;
+			return "redirect:/account/" + accountNumber;
 		}
 
 		transactionCompletion = true;
@@ -100,19 +101,19 @@ public class TransactionController {
 		transactionRepo.save(transactionRecord);
 		accountRepository.save(account.get());
 
-		return "redirect:/account/transaction/display?accountNum=" + accountNumber;
+		return "redirect:/account/" + accountNumber;
 	}
 
 	// Main Display Page for transaction history
-	@GetMapping("/display")
-	public String display(@Param("accountNum") Long accountNum,
+	@GetMapping("/{accountNum}")
+	public String display(@PathVariable("accountNum") Long accountNum,
 			@RequestParam(value = "page", defaultValue = "1") Integer page,
 			@RequestParam(value = "pageLimit", defaultValue = "10") Integer pageLimit,
 			Model model) {
 				
 		// fetch current account details
 		Optional<Account> account = accountRepository.findById(accountNum);
-		account.ifPresent(x -> model.addAttribute("accountNumber", accountNum));
+		account.ifPresent(x -> model.addAttribute("num", accountNum));
 		account.ifPresent(x -> model.addAttribute("currentBalance", x.getBalance()));
 		account.ifPresent(x -> model.addAttribute("accountNRIC", x.getCustomer().getNric().substring(5)));
 		account.ifPresent(x -> model.addAttribute("accountDetails", x.getCustomer()));
@@ -133,6 +134,7 @@ public class TransactionController {
 		model.addAttribute("pageNumber", paging.getPageNumber() + 1);
 		model.addAttribute("pageLimit", pageLimit);
 		model.addAttribute("lastPage", records.map(as -> as.getTotalPages()).orElse(null));
+		model.addAttribute("BASE_URL", "account");
 
 		return "TransactionHistory";
 	}
