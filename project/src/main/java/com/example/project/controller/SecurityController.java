c62dc12a712066cbe131f056a81ceb29cda4cf54
Keith <KeithTan@localhost>
2022-12-01 09:32:40 +0800

    Prevent going back to login page after logging in

--- project/src/main/java/com/example/project/controller/SecurityController.java
+++ project/src/main/java/com/example/project/controller/SecurityController.java
@@ -1,15 +1,25 @@
 package com.example.project.controller;
 
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class SecurityController {
 	@GetMapping("/login")
-		public String viewLoginPage() {
-			return "login";
-		}
+	public String showLoginForm(Model model) {
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+	 
+	    return "redirect:/accounts";
+	}
 }
 
